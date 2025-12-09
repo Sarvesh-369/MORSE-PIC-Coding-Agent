01b47211829430921ca06f14b7f6706b11536e49
Sarvesh <sarvesh@Sarveshs-MacBook-Pro-1187.local>
2025-12-09 14:51:18 +0530

    new pipeline

--- src/train_gepa.py
+++ src/train_gepa.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from src.code import VLMModule
-from src.metrics import visual_similarity_metric
+from src.geps_metrics import visual_similarity_metric
 
 # Try importing GEPA
 try:
